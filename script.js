07f7b75d994e0e636de556e8772b6b4b3794aae0
POGsss <frankiepinat14@gmail.com>
2024-09-02 15:03:45 +0800

    Healthcare Company

---
+++ script.js
@@ -0,0 +1,17 @@
+const mobileNav = document.querySelector('.mNav');
+const closeBtn = document.querySelector('.mNavClose');
+const closeBtnIcon = document.querySelector('.mNavIcon');
+
+const navOpen = 'left-0';
+const navClose = '-left-[280px]';
+const arrowIconLeft = 'ri-arrow-left-s-fill';
+const arrowIconRight = 'ri-arrow-right-s-fill';
+
+closeBtn.addEventListener('click', () => {
+    if (mobileNav.classList.contains(navOpen)) {
+        mobileNav.classList.toggle(navClose);
+
+        closeBtnIcon.classList.toggle(arrowIconLeft);
+        closeBtnIcon.classList.toggle(arrowIconRight);
+    }
+});
