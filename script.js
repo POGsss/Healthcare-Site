f0495a208d657342a27aadd5c841fcaa08fe68ab
POGsss <frankiepinat14@gmail.com>
2024-09-16 00:08:13 +0800

    FAQ Section

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const navClose = 'left-[-280px]';
 const arrowIconLeft = 'ri-arrow-left-s-fill';
 const arrowIconRight = 'ri-arrow-right-s-fill';
 
+// Navigation Function
 closeBtn.addEventListener('click', () => {
     if (mobileNav.classList.contains(navOpen)) {
         mobileNav.classList.toggle(navClose);
@@ -23,4 +24,18 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-pagination',
       clickable: true
     },
+});
+
+// FAQ function
+const faqItems = document.querySelectorAll('.faqItem');
+
+faqItems.forEach((item) => {
+    const faqBtn = item.querySelector('faqBtn');
+
+    item.addEventListener('click', () => {
+        const isOpen = item.classList.toggle('open');
+        const iconClass = isOpen ? 'ri-subtract-fill' : 'ri-add-fill';
+        const iconElement = faqBtn.querySelector('i');
+        iconElement.classList = `${iconClass} text-2xl`;
+    });
 });
